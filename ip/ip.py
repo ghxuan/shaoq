bb2a7b6ad6b523449f17125df6cdaa186730b006
工作 <ghxuan@outlook.com>
2019-08-06 13:39:16 +0800

    修改ip网页，访问会有不同

--- ip/ip.py
+++ ip/ip.py
@@ -1,13 +1,21 @@
 import re
+import requests
 
-with open(r'ip.html', 'r+', encoding='utf-8') as f:
-    html = f.read()
-res = re.search('<body>(.*?)<body>', html, re.S).group(1)
-res = re.sub(
-    r'(<.*? class="KPNP">.*?</.*?>)|(<.*? class="TXYK">.*?</.*?>)|(<.*? style="display:none">.*?</.*?>)',
-    '', res.split('<br>', 1)[-1])
-res = re.sub(
-    r'<.*?>(.*?)</.*?>',
-    r'\1', res)
-res = res.replace('\n', '').split('<br>')
-print(res, len(res))
+
+def getip():
+    info = requests.get('http://shaoq.com/ip').text
+    res = re.search('<head>(.*?)</head>', info, re.S).group(1)
+    keys = re.findall(r'\.(.*?){display:none}', res)
+    res = re.search('<body>(.*?)<body>', info, re.S).group(1)
+    print(r'|'.join([f'(<.*? class="{k}">.*?</.*?>)' for k in keys]) + r'|(<.*? style="display:none">.*?</.*?>)')
+    res = re.sub(
+        r'|'.join([f'(<.*? class="{k}">.*?</.*?>)' for k in keys]) + r'|(<.*? style="display:none">.*?</.*?>)',
+        '', res.split('<br>', 1)[-1])
+    res = re.sub(
+        r'<.*?>(.*?)</.*?>',
+        r'\1', res)
+    res = res.replace('\n', '').split('<br>')
+    print(res, len(res))
+
+
+getip()
